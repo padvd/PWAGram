426c2af4134a8380e442c4277cb8e184fd8dc97b
Peter van Dijk <peter-van-dijk@ziggo.nl>
2020-03-08 16:12:45 +0100

    Modified installation flow

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,15 @@
+let deferredPrompt;
+
 if ('serviceWorker' in navigator) {
   // navigator.serviceWorker.register('../../sw.js', {scope: '../../help/'}).then(() => {
   navigator.serviceWorker.register('../../sw.js').then(() => {
     console.log('Service worker registered!');
+  });
+
+  window.addEventListener('beforeinstallprompt', event => {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
   });
 }
